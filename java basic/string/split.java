67ec9ec9ba29248f44abe77559bb585df117e7fd
zkharryhhhh <33385788+zkharryhhhh@users.noreply.github.com>
2018-12-15 22:53:42 +0800

    java

--- java basic/string/split.java
+++ java basic/string/split.java
@@ -1,11 +1,14 @@
 package indi.kai.hello;
 
 public class array {
-
+    
     public static void main (String[] args){
         String str="hello,java,zk,harry,icy";
         String s=",";
+        //拆分无数量限制
         String[] ss1=str.split(s);
+        
+        //拆分有数量限制
         String[] ss2=str.split(s,3);
         System.out.println("ss1字符数组的长度："+ss1.length);
         for(int i=0;i<ss1.length;i++){
